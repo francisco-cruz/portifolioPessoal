6f033a92e84faebd258d488f30c2cd6ea07716b6
francisco-cruz <franaraujosilva2017@gmail.com>
2022-06-30 21:31:19 -0300

    fazendo tela de projetos

---
+++ scripts/tabs.js
@@ -0,0 +1,26 @@
+function openCity(evt, cityName, nameResumo) {
+  let i, tabcontent, tablinks, resumes;
+  // ocultando os conteudos
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  // adionando classe ativa no tab button
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  // mostrando o elemento ativo na tela
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+  // pegando o resumo pelo id
+  resumes = document.getElementsByClassName("resumes");
+  for (i = 0; i < resumes.length; i++) {
+    resumes[i].style.display = "none";
+  }
+  // mostrando resumo
+  document.getElementById(nameResumo).style.display = "block";
+  
+}
+
+document.getElementById("defaultOpen").click();
